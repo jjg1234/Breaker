874068ecb2dd00bac507f8cb8bbcdc767b9e0565
JJG <jjeromegerstch@gmail.com>
2017-03-12 22:40:30 +0100

    Initial sending : course 5-68

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// This class allows us to handle Levels (load,...)
+/// </summary>
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string _levelName)
+	{
+		SceneManager.LoadScene(_levelName);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+
+}
